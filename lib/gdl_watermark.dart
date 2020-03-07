fe3d635ad82b88fb64b41f719c5bc3646a56a539
Flutter Guadalajara <crystaldonut@gmail.com>
2020-03-06 19:00:53 -0600

    Initial Watermark Commit

---
+++ lib/gdl_watermark.dart
@@ -0,0 +1,46 @@
+library gdl_watermark;
+
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// A Calculator.
+class FlutterGdl {
+
+  Widget showBottomWatermark(){
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Created with', style: GoogleFonts.dMSans(fontSize: 15)),
+            SizedBox(
+              width: 5,
+            ),
+            _heart(),
+            SizedBox(
+              width: 5,
+            ),
+            Text('for Flutter Guadalajara', style: GoogleFonts.dMSans(fontSize: 15)),
+          ],
+        ),
+        // se puede haber usado safe area en todo el app
+        // pero se hubiera visto mal arriba
+        SizedBox(
+          height: 20,
+        )
+      ],
+    );
+
+  }
+
+  Widget _heart({double width = 20, double height = 20}) {
+    return Image.asset(
+      "assets/corazon.png",
+      width: width,
+      height: height,
+    );
+  }
+
+
+}
+
